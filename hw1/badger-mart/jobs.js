624643312b5e53d6d7c050f96f3803c615842e07
psycho-ken00 <czf000731@163.com>
2026-01-31 22:15:44 -0600

    Complete HW1 Badger Mart implementation

--- hw1/badger-mart/jobs.js
+++ hw1/badger-mart/jobs.js
@@ -1,5 +1,26 @@
+const job = ["Cashier", "Stocker", "Janitor"];
+
 function submitApplication(e) {
     e.preventDefault(); // You can ignore this; prevents the default form submission!
 
     // TODO: Alert the user of the job that they applied for!
+    const selectedJob = document.getElementsByName("job");
+
+    let hasSelected = false;
+    let selectedJobTitle = null;
+
+    for (const job of selectedJob) {
+        if (job.checked) {
+            hasSelected = true;
+            selectedJobTitle = job.value;
+            break;
+        }
+    }
+
+    if (hasSelected) {
+        alert("Thank you for applying to be a " + selectedJobTitle + "!");
+    }
+    else {
+        alert("Please select a job!");
+    }
 }
