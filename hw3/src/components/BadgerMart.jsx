6bad9f64ba82d7783e11c58100bfe912f1e1abe2
psycho-ken00 <czf000731@163.com>
2026-02-23 10:49:04 -0600

    Add hw3 BadgerID

--- hw3/src/components/BadgerMart.jsx
+++ hw3/src/components/BadgerMart.jsx
@@ -7,6 +7,19 @@ export default function BadgerMart(props) {
 
     useEffect(() => {
         // TODO I should fetch and setFeature here!
+        fetch("https://cs571api.cs.wisc.edu/rest/s26/hw3/featured-item", {
+        headers: {
+            "X-CS571-ID": CS571.getBadgerId()
+        }
+        })
+        .then(res => res.json())
+        .then(data => {
+            setFeature(data);
+            console.log(data);
+        })
+
+        
+
     }, []);
 
     return <div>
--- hw3/src/components/FeaturedItem.jsx
+++ hw3/src/components/FeaturedItem.jsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { Button, Card, Table } from "react-bootstrap";
+
+
 export default function FeaturedItem(props) {
-    return <div>
-        <p>I should display the feature that was passed to me...</p>
-    </div>
+    
+    const [show, setShow] = useState(false);
+
+
+    return <Card style={{margin: "auto", marginTop: "1rem", maxWidth: "25rem"}}>
+        {/* <p>I should display the feature that was passed to me...</p> */}
+        <img src={props.img} alt={props.description}/>
+        <p style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{props.name}</p>
+        <p><strong>${props.price} per unit</strong></p>
+        <p style={{ fontSize: "1rem" }}>{props.description}</p>
+        {show && <>
+            <p style={{ fontWeight: "bold" }}>Nutrition Facts</p>
+            <Table style={{margin: "auto"}}>
+                <thead>
+                    <tr>
+                        <th>Calories</th>
+                        <th> Fat</th>
+                        <th>Carbohydrates</th>
+                        <th>Protein</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{props.nutrition.calories}</td>
+                        <td>{props.nutrition.fat ? props.nutrition.fat : "0g"}</td>
+                        <td>{props.nutrition.carbohydrates ? props.nutrition.carbohydrates : "0g"}</td>
+                        <td>{props.nutrition.protein ? props.nutrition.protein : "0g"}</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>}
+        <Button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"} Nutrition Facts</Button>
+    </Card>
 }
