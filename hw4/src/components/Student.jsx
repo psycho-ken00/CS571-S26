c12f7c0f5d9db925307283999ec808a99e6209b4
psycho-ken00 <czf000731@163.com>
2026-02-23 11:05:34 -0600

    Update hw4 latest bersion

--- hw4/src/components/Student.jsx
+++ hw4/src/components/Student.jsx
@@ -1,7 +1,15 @@
+
 const Student = (props) => {
     return <div>
         <h2>{props.name.first} {props.name.last}</h2>
         {/* TODO Student data goes here! Do NOT modify the element above that displays the name.*/}
+        <strong>{props.major}</strong>
+        <br /><br />
+        <p>{props.name.first} is taking {props.numCredits} credits and {props.fromWisconsin ? "is" : "is NOT"} from Wisconsin.</p>
+        <p>They have {props.interests.length} interests including...</p>
+        <ul>
+            {props.interests.map(i => <li key={i}>{i}</li>)}
+        </ul>
     </div>
 }
 
