4002f7f6658f7dc61aedaf1d1e0902a35493510c
psycho-ken00 <czf000731@163.com>
2026-03-25 15:47:19 -0500

    Add hw8

---
+++ hw8/src/components/navigation/BadgerTabs.jsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+function BadgerTabs(props) {
+    return <>
+        <Text style={{paddingTop: 128, textAlign: "center"}}>Hello World! I should make this tabs instead.</Text>
+    </>
+}
+
+export default BadgerTabs;
